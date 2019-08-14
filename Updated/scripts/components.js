1f235f92e8b142223e28079269da871c7b7c81c9
jjheipz24 <heipler.jj@gmail.com>
2019-08-13 23:56:09 -0400

    tested bootstrap 'collapse' component

--- Updated/scripts/components.js
+++ Updated/scripts/components.js
@@ -17,3 +17,22 @@ Vue.component('about-card', {
                 </b-card>
             </div>`
 });
+
+Vue.component('project-card', {
+    props: ['id', 'img', 'title', 'descrip', 'link-descrip', 'link', 'tech'],
+    template: `<div>
+                <b-card v-b-toggle.{{id}} img-src={{img}} img-top class="project-card">
+                    <h2 class="card-title">{{title}}</h2>
+                </b-card>
+                <b-collapse id={{id}} class="mt-2">
+                    <b-card>
+                        <b-card-text><p>{{descrip}}</p>
+                            <br/><br/>
+                            <h5>Created with: {{tech}}</h5>
+                            <br/><br/>
+                            <h5>{{link-descrip}}: {{link}}</h5>
+                        </b-card-text>
+                    </b-card>
+                </b-collapse>
+            </div>`
+});
