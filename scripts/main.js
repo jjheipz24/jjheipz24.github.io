d7bd7bd9da751f026ea534f7ae353f8b1a73548d
Jin Jin Heipler <heipler.jj@gmail.com>
2020-05-13 15:07:25 -0400

    added smooth scrolling behavior

--- scripts/main.js
+++ scripts/main.js
@@ -108,6 +108,18 @@ $(".mm-doc").paroller({
     direction: 'vertical'
 });
 
+$('a[href*="#"]').on('click', function(e) {
+    e.preventDefault()
+  
+    $('html, body').animate(
+      {
+        scrollTop: $($(this).attr('href')).offset().top,
+      },
+      400,
+      'linear'
+    )
+  })
+
 
 /*Controls card flip*/
 $("#goals").flip({
