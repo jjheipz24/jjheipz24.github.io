2a8cb62773d4175a83a6b68975ebc6fbb3ddec07
Jin Jin Heipler <heipler.jj@gmail.com>
2020-05-14 15:00:18 -0400

    re-making mm page with css grid

--- scripts/main.js
+++ scripts/main.js
@@ -39,7 +39,7 @@ let app = new Vue({
                 img: 'imgs/dna.png',
                 title: "DNA Portal",
                 descrip: "For my internship at MassMutual, I worked on the UI for their Finance Data and Analytics Portal. This portal allows for a more efficient way for use-case owners and businesses to access and report finance data.",
-                link: "https://jjheipz24.github.io/mm.html",
+                link: "https://jjheipz24.github.io/",
                 tech: "HTML, CSS, Angular.js",
                 tag: "dna"
             },
